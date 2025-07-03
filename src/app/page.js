1b2ddf0db052443fb41549da770c3d67d49f2dc0
apurvkolte10 <apurvkolte10@gmail.com>
2025-07-03 18:02:45 +0530

    m

--- src/app/page.js
+++ src/app/page.js
@@ -195,14 +195,14 @@ const index = () => {
           </tbody>
         </table>
 
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Update Message</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="exampleModalLabel">Update Message</h1>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
-              <div class="modal-body">
+              <div className="modal-body">
                 <div className=" mx-auto  p-4 d-flex flex-column justify-content-center align-items-center">
                   {isSubmitSuccessful && (
                     <div className="alert alert-success w-100">
@@ -279,8 +279,8 @@ const index = () => {
                   </form>
                 </div>
               </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
 
               </div>
             </div>
